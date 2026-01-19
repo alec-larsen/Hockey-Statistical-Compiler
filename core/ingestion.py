cada1f1dbe3ec1664d6cbd796b89f431403a2422
Mtnfx <alec.larsen521@gmail.com>
2026-01-19 17:24:43 -0500

    core: add gtd - counting total games already completed in current regular season.

--- core/ingestion.py
+++ core/ingestion.py
@@ -1,6 +1,5 @@
 import json
-
-#import pandas as pd
+import datetime
 
 import requests
 from core import constants
@@ -26,3 +25,32 @@ def write_play_by_play(game_id: int, loud: bool = CONNECTION_SUCCESS_MESSAGE) ->
 
     with open(constants.ROOT_DIRECTORY / "data" / "play-by-play" /f"{game_id}.json", mode = "w", encoding="utf-8") as file:
         json.dump(pbp_json, file, indent=2)
+
+def gtd(today: datetime.date = datetime.date.today(), loud: bool = CONNECTION_SUCCESS_MESSAGE) -> int:
+    """
+    Find number of games up to, but not including today.
+
+    Returns:
+        int: Number of games that have occurred so far in the NHL regular season.
+    """
+    yesterday = str(today - datetime.timedelta(days=1))
+
+    try:
+        sched = requests.get(f"https://api-web.nhle.com/v1/schedule/{yesterday}", timeout=10)
+        if sched.status_code != 200:
+            raise ConnectionError(f"\033[91mCall to NHL API should yield status code 200, obtained status code {sched.status_code} instead.\033[0m") # pylint: disable=line-too-long
+        sched_json = sched.json() #This gives us a dictionary of the week of games containing yesterday.
+    except TimeoutError as exc:
+        raise TimeoutError("\033[91mResponse not received for API call. Please check connection.\033[0m") from exc
+
+    if loud:
+        print("\033[92mPull of schedule to date for curretn season successful!\033[0m")
+
+    day = [d for d in sched_json["gameWeek"] if d["date"] == yesterday][0] #Obtain only yesterday's game data.
+
+    #Since last four digits of game_id represent regular season order, so largest game_id is most recent game.
+    #Find maximum game_id in yesterday's game data
+    max_game_id = max([game["id"] for game in day["games"]])
+
+    #Last four digits of maximum game_id yield number of games taht have happened so far in the regular season.
+    return max_game_id % 10000
--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -1,21 +1,30 @@
 import json
+from datetime import date
 
 import pytest
 
-from core.ingestion import write_play_by_play
+from core import ingestion
 from core import constants
 
 @pytest.mark.parametrize("game_id", [
     2025020767, #Sample game from 2025-2026 season (1/19/2026)
     2025020716, #Sample game from 2025-2026 season (1/12/2026)
     2024020437, #Sample game from 2024-2025 season (12/08/2024)
-    2023030127, #Sample playoff game (05/02/2024)
+    2023030127, #Sample playoff game (05/04/2024)
 ])
 
 def test_write_play_by_play(game_id: int):
-    write_play_by_play(game_id)
+    ingestion.write_play_by_play(game_id)
     #Open file previous call should have created.
     with open(constants.ROOT_DIRECTORY / "data" / "play-by-play" / f"{game_id}.json", mode="r", encoding="utf-8") as file:
         data = json.load(file)
 
     assert data["id"] == game_id
+
+@pytest.mark.parametrize("today, n_games", [
+    (date(2026, 1, 19), 769),
+    (date(2025, 10, 15), 56),
+    (date(2025, 12, 31), 621),
+])
+def test_gtd(today: date, n_games: int):
+    assert ingestion.gtd(today = today) == n_games

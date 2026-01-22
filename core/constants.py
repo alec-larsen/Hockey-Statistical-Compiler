9eeb05375ae46022f5b096b70af0f00194e173b3
Mtnfx <alec.larsen521@gmail.com>
2026-01-21 20:29:35 -0500

    fix: add constant for dict keys kept in play-by-play cleaning

--- core/constants.py
+++ core/constants.py
@@ -2,3 +2,8 @@ from pathlib import Path
 
 #Root directory of project on local system
 ROOT_DIRECTORY = Path(__file__).resolve().parents[1]
+
+#Dictionary keys to be kept in the first step of cleaning. All other keys are 'obsolete' and will be deleted.
+#Some keys contain information obtainable from kept keys (e.g. season from first four digits of game_id)
+#Others contain information not planned to be used in the model (e.g. startTimeUTC)
+KEEP_PBP = ["id", "gameDate", "awayTeam", "homeTeam", "gameOutcome", "plays", "rosterSpots"]

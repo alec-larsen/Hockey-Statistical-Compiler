bc8715d082df013f6490c606d0775a55ea0483a7
Mtnfx <alec.larsen521@gmail.com>
2026-01-22 17:58:38 -0500

    core: constants - defined specific keys to draw details, main players, and opposing plays from by event typeCode

--- core/constants.py
+++ core/constants.py
@@ -8,3 +8,36 @@ ROOT_DIRECTORY = Path(__file__).resolve().parents[1]
 #Others contain information not planned to be used in the model (e.g. startTimeUTC)
 KEEP_PBP = ["id", "gameDate", "awayTeam", "homeTeam", "gameOutcome", "plays", "rosterSpots"]
 KEEP_TEAM = ["id", "abbrev"]
+
+#Some keys in the cleaned play-by-data are dependent on event type.
+#For example, oppPlayer comes from goalieId for goals, but playerId of winnign player for faceoffs
+#Key that should be analyzed for the details key of our cleaned dictionary (by typeCode)
+DETAIL_KEYS = {
+    505: "shotType",
+    506: "shotType",
+    507: "shotType",
+    508: "reason",
+    509: "typeCode"
+}
+
+MAIN_PLAYER_KEYS = {
+    502: "winningPlayerId",
+    503: "hittingPlayerId",
+    504: "playerId",
+    505: "scoringPlayerId",
+    506: "shootingPlayerId",
+    507: "shootingPlayerId",
+    508: "shootingPlayerId",
+    509: "comittedByPlayerId",
+    525: "playerId"
+}
+
+OPP_PLAYER_KEYS = {
+    502: "losingPlayerId",
+    503: "hitteePlayerId",
+    505: "goalieInNetId",
+    506: "goalieInNetId",
+    507: "goalieInNetId",
+    508: "blockingPlayerId",
+    509: "drawnByPlayerId"
+}

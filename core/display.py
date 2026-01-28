cbc6a842593acbf06d0288c98fb432aec71d8d28
Mtnfx <alec.larsen521@gmail.com>
2026-01-27 19:00:10 -0500

    inter: add banner printing function; add constant for length of banner lines

--- core/display.py
+++ core/display.py
@@ -1,5 +1,10 @@
+from core.constants import BANNER_LENGTH
+
 def get_menu_option():
     return input("Please choose one of the following functionalities to execute:\n"
       + "1. Ingestion pass: Ingest 100 play-by-plays from the NHL API\n"
       + "2. Cleaning pass: Clean all previously ingested raw data.\n"
       + "3. Exit.\n")
+
+def print_banner(message: str):
+    print("-"*BANNER_LENGTH + "\n" + message + "\n" + "-" * BANNER_LENGTH)

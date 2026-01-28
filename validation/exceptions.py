e2833831060e2d4b63a2e29b5f9c837c1e4592bc
Mtnfx <alec.larsen521@gmail.com>
2026-01-28 15:17:23 -0500

    valid: add two additional custom error types; NetworkError and ExcessDelayError

--- validation/exceptions.py
+++ validation/exceptions.py
@@ -1,4 +1,14 @@
-#Custom class of exception to handle verification errors.
+#Custom class of exception to handle errors raised by verification functions.
 class VerificationError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+#Custom class of exception to handle errors created by network connectivity issues.
+class NetworkError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+#Custom class of exception to handle timeout-adjacent errors in reasonably expected sections of the program.
+class ExcessDelayError(Exception):
     def __init__(self, message: str):
         super().__init__(message)
